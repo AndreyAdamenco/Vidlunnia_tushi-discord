d418e00453a78db3fdacad0570dab50c3864269e
sans228 <a.andrey.andreevitch07@gmail.com>
2023-10-29 23:16:11 +0100

    First files :3

---
+++ bot.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Відлуння_beta1
+{
+    internal class bot
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Comands { get; set; }
+        static void Main(string[] args) 
+        {
+
+        }
+    }
+}
